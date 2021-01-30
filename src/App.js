43fc57dfa305542b903d2aa525e387d345b0c2f9
nschuler <nicholas.schuler@student.uts.edu.au>
2021-01-31 10:36:53 +1100

    Added basic routing template

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </>
+  )
+};
 
 export default App;
---
+++ src/pages/Repository.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Repository = () => {
+    return (
+        <div>
+            <h1>I am repository</h1>
+        </div>
+    )
+}
+
+export default Repository
