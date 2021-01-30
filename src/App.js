605cfee91fe043810c9ecf3604684f7bcf073144
nschuler <nicholas.schuler@student.uts.edu.au>
2021-01-31 10:58:40 +1100

    Global Styling added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
 
+import GlobalStyle from './styles/global';
+
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes />
       </BrowserRouter>
+      <GlobalStyle/>
     </>
   )
 };
