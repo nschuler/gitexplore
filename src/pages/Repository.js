88e7c0e271dfc3856175e00bdaa866e23643e437
nschuler <nicholas.schuler@student.uts.edu.au>
2021-01-31 15:00:27 +1100

    Repositories Page Header

--- src/pages/Repository.js
+++ src/pages/Repository.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+
+import api from '../api/api';
+
+import logoImg from '../assets/logo.svg';
 
 const Repository = () => {
+    const [repository, setRepository] = useState(null);
+    const [issues, setIssues] = useState([]);
+
     return (
-        <div>
-            <h1>I am repository</h1>
-        </div>
+        <>
+            <Header>
+                <img src={logoImg} alt="GitHub Explorer" />
+
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+                    Back
+                </Link>
+            </Header>
+        </>
     )
 }
 
 export default Repository
+
+const Header = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    a {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: #a8a8b3;
+        text-decoration: none;
+        transition: 0.2s;
+        &:hover {
+            color: #666;
+        }
+        svg {
+            margin-right: 4px;
+        }
+    }
+`
